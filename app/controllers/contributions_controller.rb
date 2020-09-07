de8f42ffc41b3f8c1d2147c954374bab45213a0f
Haruka Teduka <th_kiyose@outlook.com>
2020-09-07 16:43:46 +0900

    topビューを作成

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,4 @@
 class ContributionsController < ApplicationController
+  def top
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  root to: "contributions#index"
+  resources :contributions do
+    collection do
+      get :top
+    end
+  end
 end
