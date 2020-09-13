26d6bc3109b2e0480b1e2d3b4268a58881463cf2
Haruka Teduka <th_kiyose@outlook.com>
2020-09-13 21:07:56 +0900

    コントローラーによってビューのスクロールを禁止するようヘルパーメソッドを作成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def is_fixed_footer
     "fixed" if controller.controller_name == "registrations"
   end
+
+  def not_to_scroll
+    "not_scroll" if controller.controller_name == "registrations"
+  end
 end
