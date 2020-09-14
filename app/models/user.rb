fb4e7ef13bdec33eee13152c6f03299c32a673b5
Haruka Teduka <th_kiyose@outlook.com>
2020-09-14 16:15:46 +0900

    Userモデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :user_name, presence: true, length: { maximum: 20 }
 end
