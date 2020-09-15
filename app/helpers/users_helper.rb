f53044e38644a99aa71a938dd59a2ba15b0e458f
Haruka Teduka <th_kiyose@outlook.com>
2020-09-15 10:33:53 +0900

    ログイン画面のレイアウト、リンク先修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def is_fixed_footer
-    "fixed" if controller.controller_name == "registrations"
+    "fixed" if controller.controller_name == "registrations" || "sessions"
   end
 
   def not_to_scroll
-    "not_scroll" if controller.controller_name == "registrations"
+    "not_scroll" if controller.controller_name == "registrations" || "sessions"
   end
 end
