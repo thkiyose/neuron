d07414c3b2e845040c3e53c8e085846a9f56526e
Haruka Teduka <th_kiyose@outlook.com>
2020-09-16 10:24:59 +0900

    ページのスクロールを禁止するヘルパーメソッドの修正2

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def is_fixed_footer
-    "fixed" if controller.controller_name == "registrations" || "sessions"
+    "fixed" if controller.controller_name == "registrations" || controller.controller_name == "sessions"
   end
 
   def not_to_scroll
-    "not_scroll" if controller.controller_name == "registrations" || "sessions"
+    "not_scroll" if controller.controller_name == "registrations" || controller.controller_name == "sessions"
   end
 end
