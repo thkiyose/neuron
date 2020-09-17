5cebbcf1a5edcd46c054896fbf02773784151147
Haruka Teduka <th_kiyose@outlook.com>
2020-09-17 17:36:26 +0900

    contributionsテーブルのuser_idカラムにNotNull制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_083121) do
+ActiveRecord::Schema.define(version: 2020_09_17_083506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_083121) do
   create_table "contributions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_contributions_on_user_id"
   end
 
