0f6d3906f7fecd12ab64adc8f5a73a4a529c6b78
Haruka Teduka <th_kiyose@outlook.com>
2020-09-17 17:33:41 +0900

    contributionsテーブルにuseridカラムを追加

---
+++ db/migrate/20200917083121_add_column_user_id_to_contributions.rb
@@ -0,0 +1,5 @@
+class AddColumnUserIdToContributions < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :contributions, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_073541) do
+ActiveRecord::Schema.define(version: 2020_09_17_083121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_09_17_073541) do
   create_table "contributions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contributions_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -47,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_073541) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "contributions", "users"
   add_foreign_key "posts", "contributions"
   add_foreign_key "profiles", "users"
 end
