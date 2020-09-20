2d1effaff94d74cf5151d045c3a66ba0d22b7cd5
Haruka Teduka <th_kiyose@outlook.com>
2020-09-20 09:35:00 +0900

    エラー解消のためhomeアクションの分岐の書き方を修正

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -3,9 +3,12 @@ class ContributionsController < ApplicationController
   end
 
   def home
-    redirect_to top_contributions_path unless user_signed_in?
-    @post = Post.new
-    @contributions = Contribution.all.includes(:user,:post).home_contributions(current_user.id)
+    unless user_signed_in?
+      redirect_to top_contributions_path
+    else
+      @post = Post.new
+      @contributions = Contribution.all.includes(:user,:post).home_contributions(current_user.id)
+    end
   end
 
   def news
--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,5 +2,5 @@ class Contribution < ApplicationRecord
   belongs_to :user
   has_one :post, dependent: :destroy
 
-  scope :home_contributions, -> (current_user_id){ where(user_id: current_user_id) if current_user_id.present? }
+  scope :home_contributions, -> (current_user_id){ where(user_id: current_user_id) if current_user_id }
 end
