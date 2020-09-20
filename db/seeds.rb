d41b7cecd5623e9d06f5f0c70a6addd2ac1b853a
Haruka Teduka <th_kiyose@outlook.com>
2020-09-20 17:37:07 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+content_sample = ["テストをする","シードデータを作る","Railsを楽しむ"]
+
+user1 = User.create(user_name:"利用者1",email:"test1@test.com",password:"password",password_confirmation:"password",admin:false)
+user2 = User.create(user_name:"利用者2",email:"test2@test.com",password:"password",password_confirmation:"password",admin:false)
+user3 = User.create(user_name:"利用者3",email:"test3@test.com",password:"password",password_confirmation:"password",admin:false)
+users = [user1,user2,user3]
+n = 0
+
+users.each do |user|
+  n += 1
+  user.create_profile(screen_name:"テストユーザー#{n}",introduction:"テスト用のユーザーデータです")
+end
+
+20.times do |n|
+  users.each do |user|
+    contribution = user.contributions.create
+    contribution.create_post(content:"#{content_sample.sample}+#{n}")
+  end
+end
