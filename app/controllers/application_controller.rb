b564929aaa664ea49221941c265abcda4a9500f9
Haruka Teduka <th_kiyose@outlook.com>
2020-09-20 21:52:39 +0900

    ransackによるpost内容への検索機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
   protect_from_forgery with: :exception
+
+  def set_search
+    @search = Contribution.ransack(params[:q])
+    @searched_contributions = @search.result(distinct: true)
+  end
 end
