4c8433b5e0c6b571b2bd4e9d2a133912505984f0
Haruka Teduka <th_kiyose@outlook.com>
2020-09-20 14:55:12 +0900

    プロフィール編集機能の実装・フラッシュメッセージがヘッダーに隠れる問題を修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,26 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: %i(show)
+  before_action :set_profile, only: %i(show edit update)
 
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, notice: "プロフィールを更新しました。"
+    else
+      render :edit
+    end
+  end
+
   private
     def set_profile
-      @user = User.find_by(id: params[:id])
-      @profile = @user.profile
+      @profile = Profile.find(params[:id])
+    end
+
+    def profile_params
+      params.require(:profile).permit(:screen_name,:introduction)
     end
 end
