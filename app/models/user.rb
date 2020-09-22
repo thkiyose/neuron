7156ff29a931cc1b0d1b8f48a9638d4b5a6b4899
Haruka Teduka <th_kiyose@outlook.com>
2020-09-22 17:25:42 +0900

    User_relationモデル作成・アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :user_name, presence: true, length: { maximum: 20 }
   has_one :profile, dependent: :destroy
   has_many :contributions, dependent: :destroy
+  has_many :active_relationships, foreign_key: "follower_id", class_name: "User_relation", dependent: :destroy
+  has_many :passive_relationships, foreign_key: "followed_id", class_name: "User_relation", dependent: :destroy
+  has_many :following, through :active_relationships, source :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_083506) do
+ActiveRecord::Schema.define(version: 2020_09_22_035132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2020_09_17_083506) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "user_relations", force: :cascade do |t|
+    t.bigint "follower_id"
+    t.bigint "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_user_relations_on_followed_id"
+    t.index ["followed_id"], name: "index_user_relations_on_followed_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_user_relations_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
