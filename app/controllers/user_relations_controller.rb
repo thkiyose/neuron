db07b0cac32b63d40a5fb75d4f65cd1bc2aa97e9
Haruka Teduka <th_kiyose@outlook.com>
2020-09-23 09:58:07 +0900

    フォロー機能の実装

--- app/controllers/user_relations_controller.rb
+++ app/controllers/user_relations_controller.rb
@@ -1,7 +1,14 @@
 class UserRelationsController < ApplicationController
+  before_action :authenticate_user!
+  redpond_to? :js
+
   def create
+    @user = User.find(params[:user_relation][:followed_id])
+    current_user.follow!(@user)
   end
 
   def destroy
+    @user = User_relation.find(params[:id]).followed
+    current_user.unfollow!(@user)
   end
 end
