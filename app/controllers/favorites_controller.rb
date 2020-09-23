9c952a86e1336b7988128565804651d9a306f65b
Haruka Teduka <th_kiyose@outlook.com>
2020-09-23 15:12:46 +0900

    お気に入り追加ボタンの設置とアクションの実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,11 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  respond_to? :js
+
   def create
+    @contribution = Contribution.find(params[:favorite][:contribution_id])
+    favorite = current_user.favorites.create(contribution_id: @contribution.id)
+    flash.now[:notice] = "投稿をお気に入りしました。"
   end
 
   def destroy
