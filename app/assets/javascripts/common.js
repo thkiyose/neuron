c74ad559f76a1e3b5c724dead0ce4954039b7db3
Haruka Teduka <th_kiyose@outlook.com>
2020-09-24 23:22:23 +0900

    コメント欄の開閉で投稿表示部下の記述が変わるようにjsを記述

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,7 +1,15 @@
 $(function(){
   $(".notifications").fadeOut(2000);
+  var message_flg = "hidden"
   $(".comments").hide();
   $(".comments_open").on("click", function(){
+    if(message_flg == "hidden"){
+      $(this).find("#open_message").text("▲コメントを隠す");
+      message_flg = "open";
+    } else {
+      $(this).find("#open_message").text("▼コメントを表示");
+      message_flg = "hidden";
+    }
     $(this).siblings(".comments").slideToggle();
   });
 });
