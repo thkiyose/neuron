14a187ed986ebbf29bf452d37b945310a6b9e5a1
Haruka Teduka <th_kiyose@outlook.com>
2020-09-24 23:05:24 +0900

    コメント欄の開閉機能を実装

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,3 +1,7 @@
 $(function(){
   $(".notifications").fadeOut(2000);
+  $(".comments").hide();
+  $(".comments_open").on("click", function(){
+    $(this).siblings(".comments").slideToggle();
+  });
 });
