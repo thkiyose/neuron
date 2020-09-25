bcb5ff768b884ecfe1b0baaedca9649472362f2d
Haruka Teduka <th_kiyose@outlook.com>
2020-09-25 12:26:08 +0900

    que用のフォームを作成

--- app/models/que.rb
+++ app/models/que.rb
@@ -2,5 +2,5 @@ class Que < ApplicationRecord
   belongs_to :contribution
   has_many :options, dependent: :destroy
   validates :question, length: { maximum: 500 }, presence: true
-  enum que_type: %i(選択肢　自由回答)
+  enum que_type: %i(options answers)
 end
