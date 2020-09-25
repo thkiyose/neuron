78b9e2611620399a16f04cf64ee74c57a693aae8
Haruka Teduka <th_kiyose@outlook.com>
2020-09-25 11:21:45 +0900

    que周りのモデルにバリデーション設定

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,4 @@
 class Option < ApplicationRecord
   belongs_to :que
+  validates :option_name, length: { maximum: 30 }, presence: true
 end
--- app/models/que.rb
+++ app/models/que.rb
@@ -1,3 +1,5 @@
 class Que < ApplicationRecord
   belongs_to :contribution
+  validates :question, length: { maximum: 500 }, presence: true
+  enum que_type: %i(選択肢　自由回答)
 end
