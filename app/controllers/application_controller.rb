f8f1d0d4b8da069d461617d11a1cb20621d9527e
Haruka Teduka <th_kiyose@outlook.com>
2020-09-26 11:04:10 +0900

    queのquestionも検索対象に追加 close #11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     @search = Contribution.includes(:post, [que: :options], [user: [:profile, :favorites, :comments]]).ransack(params[:q])
     @searched_contributions = @search.result(distinct: true).order(created_at: :desc)
     if params[:q].present?
-      @title = "キーワード\"#{params[:q][:post_content_cont]}\"の検索結果を表示しています"
+      @title = "キーワード\"#{params[:q][:post_content_or_que_question_cont]}\"の検索結果を表示しています"
     else
       @title = "新着投稿を表示しています"
     end
