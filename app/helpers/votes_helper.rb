b8b4eba1c83839e0ec018ec3573b9a1e155e7b88
Haruka Teduka <th_kiyose@outlook.com>
2020-09-26 10:50:54 +0900

    投票結果の表示機能を実装

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,8 +1,7 @@
 module VotesHelper
   def already_voted?(contribution)
-    if user_signed_in?
-      vote = Vote.find_by(user_id: current_user.id)
-      vote.option.que.contribution_id == contribution.id
-    end
+    return false unless user_signed_in?
+      votes = Vote.where(user_id: current_user.id)
+      votes.where(option_id: contribution.que.options.ids).present?
   end
 end
