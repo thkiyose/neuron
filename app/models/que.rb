a9e5d43591204bc79e15a1828df1a12400cb506c
Haruka Teduka <th_kiyose@outlook.com>
2020-09-27 13:54:54 +0900

    投稿フォームのレイアウト調整

--- app/models/que.rb
+++ app/models/que.rb
@@ -2,6 +2,6 @@ class Que < ApplicationRecord
   belongs_to :contribution
   has_many :options, dependent: :destroy
   validates :question, length: { maximum: 500 }, presence: true
-  enum que_type: %i(options answers)
+  enum que_type: %i(options)
   accepts_nested_attributes_for :options, allow_destroy: true
 end
